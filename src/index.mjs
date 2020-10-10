8166e5aa0e6d5606df25c0b8bfe945158f6be92b
William Buttenham <connorbu@gmail.com>
2020-10-09 23:28:47 -0400

    new characters

--- src/index.mjs
+++ src/index.mjs
@@ -5,12 +5,14 @@ import boxen from 'boxen'
 
 const borderStyle = 'classic'
 
-function say(string) {
+function say(string, color = 'default-parrot') {
+  const filename = color + '.txt';
   return new Promise((resolve, reject) => {
-    readFile(joinPath(__dirname, 'default-parrot.txt'), 'utf-8', (err, data) => {
+    readFile(joinPath('./src/parrots', filename), 'utf-8', (err, data) => {
       if (err) {
         reject(err)
       }
+
       resolve(boxen(string, {borderStyle}) + data)
     })
   })
