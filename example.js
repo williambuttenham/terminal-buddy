fb7b8deafbb3bf5750d33371dd15ca68066c0028
William Buttenham <connorbu@gmail.com>
2020-10-09 23:29:33 -0400

    rename

--- example.js
+++
@@ -1,6 +0,0 @@
-// example.js
-const parrotSay = require('./dist')
-
-parrotSay('yaaay')
-  .then(console.log)
-  .catch(console.error)
---
+++ example.mjs
@@ -0,0 +1,5 @@
+import say from "./src/index.mjs";
+
+say('yaaay', 'head')
+  .then(console.log)
+  .catch(console.error)
