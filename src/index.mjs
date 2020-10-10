54139d721fdf1c0150841cf99cf8138f1865e277
William Buttenham <connorbu@gmail.com>
2020-10-09 23:40:48 -0400

    new folder name

--- src/index.mjs
+++ src/index.mjs
@@ -8,7 +8,7 @@ const borderStyle = 'classic'
 function say(string, color = 'default-parrot') {
   const filename = color + '.txt';
   return new Promise((resolve, reject) => {
-    readFile(joinPath('./src/parrots', filename), 'utf-8', (err, data) => {
+    readFile(joinPath('./src/buddies', filename), 'utf-8', (err, data) => {
       if (err) {
         reject(err)
       }
