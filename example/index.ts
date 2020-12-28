a177f51fe2900edec58ab266ca3d5c58e7abbb8b
William Buttenham <connorbu@gmail.com>
2020-12-28 17:47:15 -0500

    Update test to show off more than one buddy update buddies remove fake red parrot

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,10 @@
 import say from "../src/index";
 
-say('yaaay', 'head')
+say('yaaay')
   .then(console.log)
+
+say('yaaay', 'head')
+.then(console.log)
+
+say('yaaay', 'green-parrot')
+.then(console.log)
