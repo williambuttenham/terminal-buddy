05456a3d2f70c2034b0fef58823f15cba686f07b
William Buttenham <william.buttenham@gmail.com>
2020-12-28 19:55:27 -0500

    Remove gulp (#14)
    * Remove gulp
    
    * more nodes
    
    * v6 doesnt have npm ci
    
    * engines
    
    * update package
    
    * new eslint
    
    * new formatting
    
    * Ignore issues in example
    
    * types
    
    * ugh

--- example/index.ts
+++ example/index.ts
@@ -1,13 +1,16 @@
-import say from '../src';
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+import say from '../src/index';
 
 say('yaaay')
-	.then(console.log)
-	.catch(console.error);
+  .then(console.log)
+  .catch(console.error);
 
 say('yaaay', 'head')
-	.then(console.log)
-	.catch(console.error);
+  .then(console.log)
+  .catch(console.error);
 
 say('yaaay', 'green-parrot')
-	.then(console.log)
-	.catch(console.error);
+  .then(console.log)
+  .catch(console.error);
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import {join as joinPath} from 'path';
-import {readFile} from 'fs';
+import { join as joinPath } from 'path';
+import { readFile } from 'fs';
 
 async function say(string: string, color = 'default') {
-	const filename = color + '.txt';
-	return new Promise((resolve, reject) => {
-		readFile(joinPath('./src/buddies', filename), 'utf-8', (err, data) => {
-			if (err) {
-				reject(err);
-			}
+  const filename = `${color}.txt`;
+  return new Promise((resolve, reject) => {
+    readFile(joinPath('./src/buddies', filename), 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
 
-			resolve(`${data}\n${string}`);
-		});
-	});
+      resolve(`${data}\n${string}`);
+    });
+  });
 }
 
 export default say;
