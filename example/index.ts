c26dfcacb29c0c2285602e9554958d54b98d44b1
William Buttenham <connorbu@gmail.com>
2020-12-28 19:16:07 -0500

    new formatting

--- example/index.ts
+++ example/index.ts
@@ -1,13 +1,13 @@
 import say from '../src';
 
 say('yaaay')
-	.then(console.log)
-	.catch(console.error);
+  .then(console.log)
+  .catch(console.error);
 
 say('yaaay', 'head')
-	.then(console.log)
-	.catch(console.error);
+  .then(console.log)
+  .catch(console.error);
 
 say('yaaay', 'green-parrot')
-	.then(console.log)
-	.catch(console.error);
+  .then(console.log)
+  .catch(console.error);
