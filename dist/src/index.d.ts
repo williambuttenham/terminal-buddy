917f04a7350106fafc33bacf10a93fc76a2773aa
William Buttenham <connorbu@gmail.com>
2021-05-30 00:17:37 -0400

    delete

--- dist/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare enum Pal {
-    Banana = "banana.ans",
-    Default = "default.txt",
-    Parrot = "green-parrot.txt",
-    Fish = "fish.ans",
-    Skull = "skull.ans"
-}
-declare function say(string: string, pal?: Pal | string): Promise<unknown>;
-export { Pal, say };
